ee586c87b45d95356baf0668ec48bf47582808b3
Gabriele Bettineschi <gbettineschi29@gmail.com>
2024-03-20 02:03:36 +0100

    Now working

---
+++ main.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+const { IgApiClient } = require('instagram-private-api');
+const { readFile } = require('fs');
+const { promisify } = require('util');
+
+const readFileAsync = promisify(readFile);
+
+dotenv.config({ path: '.env.local' });
+
+const postToInsta = async (username, password, pathToImage, caption) => {
+	const ig = new IgApiClient();
+	ig.state.generateDevice(username);
+	const loginResult = await ig.account.login(username, password);
+	console.log(loginResult);
+
+	const publishResult = await ig.publish.photo({
+		file: await readFileAsync(pathToImage),
+		caption: caption,
+	});
+
+	console.log(publishResult);
+};
+
+postToInsta(
+	process.env.IG_GERTRUDE_USERNAME,
+	process.env.IG_GERTRUDE_PASSWORD,
+	'./propic.jpg', //worked with .jpg, didn't work with .png
+	'che bella descrizione'
+);
